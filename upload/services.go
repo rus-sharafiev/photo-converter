e71d1790ed4d0fcf5c95cf3ecf24e107a540c3d7
Rus Sharafiev <srr@live.ru>
2024-02-06 14:59:59 +0300

    Refactor to concurrently process images

--- upload/services.go
+++ upload/services.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -20,12 +21,9 @@ import (
 
 // SERVE UPLOAD -------------------------------------------------------------------
 func (c *Controller) serve(w http.ResponseWriter, r *http.Request) {
-	// if _, role := auth.Headers(r); role != "ADMIN" {
-	// 	exception.Forbidden(w)
-	// 	return
-	// }
-
-	// TODO check user id, to serve private folder
+	if _, role := auth.Headers(r); role != "ADMIN" {
+		// TODO check user id, to serve private folder
+	}
 
 	w.Header().Add("Cache-Control", "private, max-age=31536000, immutable")
 	http.StripPrefix("/upload/", http.FileServer(http.Dir(c.UploadDir))).ServeHTTP(w, r)
@@ -33,6 +31,7 @@ func (c *Controller) serve(w http.ResponseWriter, r *http.Request) {
 
 // HANDLE UPLOAD ------------------------------------------------------------------
 func (c *Controller) handle(w http.ResponseWriter, r *http.Request) {
+	const basePath = "/upload"
 
 	// Check whether request contains multipart/form-data
 	if mr, err := r.MultipartReader(); err == nil {
@@ -43,6 +42,7 @@ func (c *Controller) handle(w http.ResponseWriter, r *http.Request) {
 			subFolder = userID
 		}
 
+		// Check existence or create folders
 		sizes := [5]string{Original, Small, Medium, Large, ExtraLarge}
 		for _, size := range sizes {
 			fullPath := path.Join(c.UploadDir, subFolder, size)
@@ -62,150 +62,177 @@ func (c *Controller) handle(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		// Create map to save uploaded files urls
-		// filesUrlMap := make(map[string]interface{})
-		var galleryImageSlice []GalleryImage
+		imagesChan := make(chan GalleryImage)
+		errChan := make(chan error)
+		fileList := []string{}
 
 		// Iterate over the range of multipart file fields
-		for _, values := range form.File {
-			for _, fileHeader := range values {
+		for name, values := range form.File {
+			for i, fileHeader := range values {
+				fileList = append(fileList, name+"-"+strconv.Itoa(i))
+				fileHeader := fileHeader
 
-				// Get file
-				file, err := fileHeader.Open()
-				if err != nil {
-					exception.InternalServerError(w, err)
-					return
-				}
-
-				// Decode images
-				var img image.Image
-				switch contentType := fileHeader.Header.Get("Content-Type"); contentType {
-				case "image/png":
-					img, _ = png.Decode(file)
-				case "image/jpeg":
-					img, _ = jpeg.Decode(file)
-				case "image/heic":
-					exception.BadRequestError(w, fmt.Errorf("heic is not supported yet"))
-					return
-				default:
-					exception.BadRequestError(w, fmt.Errorf("file type is not supported. allowed are png, jpeg, heic"))
-					return
-				}
-
-				imgWidth := img.Bounds().Bounds().Max.X
-				imgHeight := img.Bounds().Bounds().Max.Y
-				aspectRatio := float32(imgWidth) / float32(imgHeight)
-
-				// Create file name
-				id, err := uuid.NewRandom()
-				if err != nil {
-					exception.InternalServerError(w, err)
-					return
-				}
-
-				fileName := id.String() + ".png" // filepath.Ext(fileHeader.Filename)
-				originalOutputLocation := path.Join(path.Join(c.UploadDir, subFolder, Original), fileName)
-
-				originalOutFile, err := os.Create(originalOutputLocation)
-				if err != nil {
-					exception.InternalServerError(w, err)
-					return
-				}
-				defer originalOutFile.Close()
-
-				if err := png.Encode(originalOutFile, img); err != nil {
-					exception.InternalServerError(w, err)
-					return
-				}
-
-				// Create sizes
-				imageSizes := make(map[string]string)
-				for _, size := range sizes[1:] {
-
-					intSize, err := strconv.Atoi(size)
+				go func() {
+					// Get file
+					file, err := fileHeader.Open()
 					if err != nil {
-						exception.InternalServerError(w, err)
+						errChan <- err
 						return
 					}
 
-					// Create output file
-					outputLocation := path.Join(path.Join(c.UploadDir, subFolder, size), fileName)
-					outFile, err := os.Create(outputLocation)
-					if err != nil {
-						exception.InternalServerError(w, err)
+					// Decode images
+					var img image.Image
+					switch contentType := fileHeader.Header.Get("Content-Type"); contentType {
+					case "image/png":
+						img, _ = png.Decode(file)
+					case "image/jpeg":
+						img, _ = jpeg.Decode(file)
+					case "image/heic":
+						errChan <- fmt.Errorf("error loading file %s: heic is not supported yet", fileHeader.Filename)
+						return
+					default:
+						errChan <- fmt.Errorf("error loading file %s: file type is not supported. png, jpeg, heic are allowed", fileHeader.Filename)
 						return
 					}
-					defer outFile.Close()
 
-					if intSize >= max(imgWidth, imgHeight) {
+					imgWidth := img.Bounds().Bounds().Max.X
+					imgHeight := img.Bounds().Bounds().Max.Y
+					aspectRatio := float32(imgWidth) / float32(imgHeight)
 
-						// Return original if smaller than target size
-						if err := png.Encode(outFile, img); err != nil {
-							exception.InternalServerError(w, err)
-							return
-						}
+					// Create file name
+					id, err := uuid.NewRandom()
+					if err != nil {
+						errChan <- err
+						return
+					}
 
-					} else {
+					fileName := id.String() + ".png" // filepath.Ext(fileHeader.Filename)
+					originalOutputLocation := ""
 
-						// Set the size
-						var dst *image.RGBA
-						if aspectRatio >= 1 {
-							dst = image.NewRGBA(image.Rect(0, 0, intSize, int(float32(intSize)/aspectRatio)))
-						} else {
-							dst = image.NewRGBA(image.Rect(0, 0, int(float32(intSize)*aspectRatio), intSize))
-						}
+					// Create sizes
+					imageSizesChan := make(chan []string)
+					errorSizesChan := make(chan error)
 
-						// Resize
-						draw.ApproxBiLinear.Scale(dst, dst.Rect, img, img.Bounds(), draw.Over, nil)
+					for _, size := range sizes {
+						size := size
 
-						// Encode to png
-						if err := png.Encode(outFile, dst); err != nil {
-							exception.InternalServerError(w, err)
-							return
+						go func() {
+
+							// Create output file
+							outputLocation := path.Join(path.Join(c.UploadDir, subFolder, size), fileName)
+							outFile, err := os.Create(outputLocation)
+							if err != nil {
+								errorSizesChan <- err
+								return
+							}
+							defer outFile.Close()
+
+							// Write the original as PNG
+							if size == Original {
+
+								if err := png.Encode(outFile, img); err != nil {
+									errorSizesChan <- err
+									return
+								}
+
+							} else {
+
+								// Get target size
+								intSize, err := strconv.Atoi(size)
+								if err != nil {
+									errorSizesChan <- err
+									return
+								}
+
+								if intSize >= max(imgWidth, imgHeight) {
+
+									// Return original if smaller than target size
+									if err := png.Encode(outFile, img); err != nil {
+										errorSizesChan <- err
+										return
+									}
+
+								} else {
+
+									// Create new image with long side target size
+									var dst *image.RGBA
+									if aspectRatio >= 1 {
+										dst = image.NewRGBA(image.Rect(0, 0, intSize, int(float32(intSize)/aspectRatio)))
+									} else {
+										dst = image.NewRGBA(image.Rect(0, 0, int(float32(intSize)*aspectRatio), intSize))
+									}
+
+									// Resize
+									draw.ApproxBiLinear.Scale(dst, dst.Rect, img, img.Bounds(), draw.Over, nil)
+
+									// Encode to png
+									if err := png.Encode(outFile, dst); err != nil {
+										errorSizesChan <- err
+										return
+									}
+								}
+							}
+							imageSizesChan <- []string{size, strings.Replace(outputLocation, c.UploadDir, basePath, 1)}
+						}()
+					}
+
+					imageSizes := make(map[string]string)
+					var errorSizes error
+
+					for range sizes {
+						select {
+						case sizes := <-imageSizesChan:
+							if sizes[0] == Original {
+								originalOutputLocation = sizes[1]
+							} else {
+								imageSizes[sizes[0]] = sizes[1]
+							}
+						case err := <-errorSizesChan:
+							errorSizes = err
 						}
 					}
 
-					imageSizes[size] = outputLocation
-				}
+					if errorSizes != nil {
+						errChan <- errorSizes
+						return
+					}
 
-				// Create Gallery Image object
-				galleryImage := GalleryImage{
-					File:         &fileName,
-					Name:         &fileHeader.Filename,
-					Url:          &originalOutputLocation,
-					OriginalSize: &fileHeader.Size,
-					Width:        &imgWidth,
-					Height:       &imgHeight,
-					Sizes:        &imageSizes,
-					CreatedAt:    time.Now(),
-				}
+					// Create Gallery Image object
+					galleryImage := GalleryImage{
+						File:         &fileName,
+						Name:         &fileHeader.Filename,
+						Url:          &originalOutputLocation,
+						OriginalSize: &fileHeader.Size,
+						Width:        &imgWidth,
+						Height:       &imgHeight,
+						Sizes:        &imageSizes,
+						CreatedAt:    time.Now(),
+					}
 
-				galleryImageSlice = append(galleryImageSlice, galleryImage)
-
-				// Add uploaded file url to map as string or append to existing slice (if multiple with the same field name)
-				// if filesUrlMap[name] == nil {
-
-				// 	filesUrlMap[name] = fileName
-
-				// } else {
-
-				// 	if existingStr, ok := filesUrlMap[name].(string); ok {
-				// 		filesUrlMap[name] = []string{existingStr, fileName}
-				// 	}
-
-				// 	if existingSlice, ok := filesUrlMap[name].([]string); ok {
-				// 		filesUrlMap[name] = append(existingSlice, fileName)
-				// 	}
-
-				// }
+					imagesChan <- galleryImage
+				}()
 			}
 
 		}
 
-		var (
-			resultJson string
-		)
+		// Get results and errors from chain
+		var galleryImageSlice []GalleryImage
+		var errorsSlice []error
 
+		for range fileList {
+			select {
+			case galleryImage := <-imagesChan:
+				galleryImageSlice = append(galleryImageSlice, galleryImage)
+			case err := <-errChan:
+				errorsSlice = append(errorsSlice, err)
+			}
+		}
+
+		for _, err := range errorsSlice {
+			fmt.Println(err)
+		}
+
+		var resultJson string
 		// Convert map with files urls to JSON object
 		if len(galleryImageSlice) != 0 {
 			filesList, err := json.Marshal(galleryImageSlice)
